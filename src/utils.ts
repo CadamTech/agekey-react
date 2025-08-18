b927321dfdd5f74ba73b395459117ae1daa649ac
Hamza Zbili <hamza.zbili.92@gmail.com>
2025-08-18 12:26:42 +0100

    Fixed environment public key prefixes

--- src/utils.ts
+++ src/utils.ts
@@ -10,12 +10,26 @@ const authUrlStage = import.meta.env.VITE_OPALE_AUTH_URL_STAGE
 const baseApiUrlProd = import.meta.env.VITE_OPALE_API_URL_PROD
 const authUrlProd = import.meta.env.VITE_OPALE_AUTH_URL_PROD
 
+const baseApiUrlDevV2 = import.meta.env.VITE_AGEKEY_API_URL_DEV
+const authUrlDevV2 = import.meta.env.VITE_AGEKEY_AUTH_URL_DEV
+
+const baseApiUrlStaingV2 = import.meta.env.VITE_AGEKEY_API_URL_STAGING
+const authUrlStagingV2 = import.meta.env.VITE_AGEKEY_AUTH_URL_STAGING
+
+const baseApiUrlV2Prod = import.meta.env.VITE_AGEKEY_API_URL_PROD
+const authUrlV2Prod = import.meta.env.VITE_AGEKEY_AUTH_URL_PROD
 
 export function getEnvironmentUrls(publicKey: string): { baseApiUrl: string, authUrl: string } {
   if (publicKey.startsWith("dev-")) {
     return { baseApiUrl: baseApiUrlDev, authUrl: authUrlDev };
   } else if (publicKey.startsWith("staging-")) {
     return { baseApiUrl: baseApiUrlStage, authUrl: authUrlStage };
+  } else if (publicKey.startsWith("devv2-")){
+    return { baseApiUrl: baseApiUrlDevV2, authUrl: authUrlDevV2 };
+  } else if (publicKey.startsWith("stagingv2-")){
+    return { baseApiUrl: baseApiUrlStaingV2, authUrl: authUrlStagingV2 };
+  } else if (publicKey.startsWith("v2-")){
+    return { baseApiUrl: baseApiUrlV2Prod, authUrl: authUrlV2Prod };
   } else {
     return { baseApiUrl: baseApiUrlProd, authUrl: authUrlProd };
   }
